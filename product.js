e6332e0c2a8056670a6e0784e0d291b4506a9474
marwanmabrouk174-cell <marwanmabrouk174@gmail.com>
2025-12-28 22:06:33 +0200

    Update product.js

--- product.js
+++ product.js
@@ -102,7 +102,7 @@ const loadSimilar = async (category) => {
 fetchProduct();
 /*/
 
-import { db } from "firebase.js";
+import { db } from "./firebase.js";
 import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const container = document.getElementById("product-details");
